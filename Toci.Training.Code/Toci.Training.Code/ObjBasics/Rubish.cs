a18aa1c87fc9fcc44d2a8a990bafdd20b12b47a7
nawr0t <navvrot@gmail.com>
2015-10-16 00:10:23 +0200

    resharper

--- Toci.Training.Code/Toci.Training.Code/ObjBasics/Rubish.cs
+++ Toci.Training.Code/Toci.Training.Code/ObjBasics/Rubish.cs
@@ -1,7 +1,20 @@
-﻿namespace Toci.Training.Code.ObjBasics
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toci.Training.Code.ObjBasics
 {
     public class Rubish
     {
-         
+        public Rubish()
+        {
+        }
+
+
+        public void test()
+        {
+            List<int> list = new List<int>();
+
+        }
+
     }
 }
