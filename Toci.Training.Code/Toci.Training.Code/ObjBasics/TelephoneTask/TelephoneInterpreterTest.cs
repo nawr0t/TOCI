3d906b5c884a9b93d5f8c00e98aef033241e4bd7
nawr0t <navvrot@gmail.com>
2015-10-24 00:49:28 +0200

    Added Test

---
+++ Toci.Training.Code/Toci.Training.Code/ObjBasics/TelephoneTask/TelephoneInterpreterTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Training.Code.ObjBasics.TelephoneTask
+{
+    [TestClass]
+    public class TelephoneInterpreterTest
+    {
+        [TestMethod]
+        public void TestTelephoneInterpreter()
+        {
+            var elements = GetAllInterpreters();
+
+            foreach (var element in elements)
+            {
+                element.GetUserChoice(2);
+            }
+        }
+
+        private IEnumerable<TelephoneInterpreter> GetAllInterpreters()
+            {
+            List<TelephoneInterpreter> objects = new List<TelephoneInterpreter>();
+                foreach (Type type in Assembly.GetAssembly(typeof(TelephoneInterpreter)).GetTypes().Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(TelephoneInterpreter))))
+                {
+                        objects.Add((TelephoneInterpreter)Activator.CreateInstance(type));
+                }
+                return objects;
+            }
+    }
+}
